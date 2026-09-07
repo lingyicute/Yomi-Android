b03638ad12f4c7147dede6dd5f11caf6266796aa
lingyicute <160479206+lingyicute@users.noreply.github.com>
2026-09-07 23:36:52 +0800

    fix build error

--- lib/pages/chat/events/html_message.dart
+++ lib/pages/chat/events/html_message.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/rendering.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter_highlighter/flutter_highlighter.dart';
 import 'package:flutter_highlighter/themes/shades-of-purple.dart';
