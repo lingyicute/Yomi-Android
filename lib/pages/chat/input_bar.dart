5e86fd491c0562bf21ab468e09e2d89081535d90
lingyicute <160479206+lingyicute@users.noreply.github.com>
2026-09-08 11:02:23 +0000

    small fixes

--- lib/pages/chat/input_bar.dart
+++ lib/pages/chat/input_bar.dart
@@ -23,7 +23,7 @@ class InputBar extends StatelessWidget {
   final ValueChanged<Uint8List?>? onSubmitImage;
   final FocusNode? focusNode;
   final TextEditingController? controller;
-  final InputDecoration? decoration;
+  final InputDecoration decoration;
   final ValueChanged<String>? onChanged;
   final bool? autofocus;
   final bool readOnly;
@@ -37,7 +37,7 @@ class InputBar extends StatelessWidget {
     this.onSubmitImage,
     this.focusNode,
     this.controller,
-    this.decoration,
+    required this.decoration,
     this.onChanged,
     this.autofocus,
     this.textInputAction,
@@ -436,7 +436,8 @@ class InputBar extends StatelessWidget {
           // it sets the types for the callback incorrectly
           onSubmitted!(text);
         },
-        decoration: decoration!,
+        maxLength: 16384,
+        decoration: decoration,
         onChanged: (text) {
           // fix for the library for now
           // it sets the types for the callback incorrectly
@@ -444,6 +445,7 @@ class InputBar extends StatelessWidget {
         },
         textCapitalization: TextCapitalization.sentences,
       ),
+
       suggestionsCallback: getSuggestions,
       itemBuilder: (c, s) => buildSuggestion(c, s, Matrix.of(context).client),
       onSelected: (Map<String, String?> suggestion) =>
